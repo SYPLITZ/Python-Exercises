62b0a8b8c5e4bef8cce14c43cc7dc4f1bdd0411b
Matheus de Paiva <matheuspaiva609@gmail.com>
2025-10-01 09:44:26 -0300

    Add files via upload

---
+++ valex1.py
@@ -0,0 +1,5 @@
+number = int(input("Digite um número inteiro: "))
+if number > 0:
+    print('Esse número é positivo!')
+else:
+    print('Esse número é negativo!')
---
+++ valex2.py
@@ -0,0 +1,8 @@
+number = int(input('Insira um número inteiro: '))
+
+if number > 0:
+    print('Esse número é positivo!')
+elif number == 0:
+    print('Esse número é o zero!')
+else:
+    print('Esse número é negativo!')
