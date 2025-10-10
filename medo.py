443bceffaf205d5e9ef6e33ad2114076d4943c60
Matheus de Paiva <matheuspaiva609@gmail.com>
2025-10-10 10:58:06 -0300

    Add files via upload

--- medo.py
+++ medo.py
@@ -4,18 +4,18 @@ def confi_triangulos(a, b, c):
     else:
         triangulo_confirmacao = 'negativo'
         return triangulo_confirmacao
-def qual_triangulo(triangulo_confirmacao):
+def qual_triangulo(triangulo_confirmacao, a, b, c):
     if triangulo_confirmacao == 'positivo':
-        if b == c != a and a == c != b and a == b != c:
+        if a == b or b == c or a == c:
             tipo_triangulo = 'isosceles'
-        elif b == a == c:
+        elif a == b and b == c:
             tipo_triangulo = 'equilatero'
-        elif b != a != c:
+        else:
             tipo_triangulo = 'escaleno'
             return tipo_triangulo
-def retangulo(a, b, c):
-    teorema = (a**2) + (b**2) 
-    if teorema == c ** 2
+def retangulo(a, b):
+    teorema = (a**2 + b**2)**0.5 
+    if teorema**2 == (a**2 + b**2):
         retangulo_confirmacao = 'positivo'
     else:
         retangulo_confirmacao = 'negativo'
@@ -29,18 +29,17 @@ b = int(input('Digite o primeiro cateto: '))
 a = int(input('Digite o segundo cateto: '))
 #variaveis funções
 triangulo_confirmacao = confi_triangulos(a, b, c)
-tipo_triangulo = qual_triangulo(triangulo_confirmacao)
-retangulo_confirmacao = retangulo(a, b, c):
+tipo_triangulo = qual_triangulo(triangulo_confirmacao, a, b, c)
+retangulo_confirmacao = retangulo(a, b)
 #execução final
-    if triangulo_confirmacao == 'positivo':
-        print(f'O seu triangulo é um triangulo {tipo_triangulo}')
-        if retangulo_confirmacao == 'positivo':
-            print('Esse triângulo é retângulo!')
-        else:
-            if retangulo_confirmacao == 'negativo':
-                print('Não é retângulo')
-
+if triangulo_confirmacao == 'positivo':
+    print(f'O seu triangulo é um triangulo {tipo_triangulo}')
+if retangulo_confirmacao == 'positivo':
+    print('Esse triângulo é retângulo!')
+else:
+    if retangulo_confirmacao == 'negativo':
+        print('Não é retângulo')
     else:
-        if triangulo_confirmacao == 'negativo':
-            print('Não é possível de se formar um triângulo...')
+       if triangulo_confirmacao == 'negativo':
+          print('Não é possível de se formar um triângulo...')
             
